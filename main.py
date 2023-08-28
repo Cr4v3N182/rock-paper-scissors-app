e3dcb0e64938e33e8512f481ef6c76a4e0c47989
i5-PC <mcienkowski@gmail.com>
2023-08-28 12:43:22 +0200

    Changed to tkinter and add main screen and labels

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
-import PySimpleGUI as sg
+from tkinter import *
 import random
 
-sg.theme("DarkAmber")
+#main screen
+master = Tk()
+master.title("RPS")
 
-score = []
-cpu_options = ["rock", "paper", "scissors"]
+#labels
+Label(master,text="rock-paper-scissors", font=("Clibri", 14)).grid(row=0, sticky=N,
+        pady=10, padx=200)
+Label(master,text="Choose something", font=("Clibri", 14)).grid(row=0, sticky=N,
+        pady=10, padx=200)
 
-def cpu_choice():
-    cpu_item = random.choice(cpu_options)
-    return cpu_item
-
-label = sg.Text("Choose stone, paper or scissors, and try to gues what's in a box")
-rock_button = sg.Button("Rock", key="rock")
-paper_button = sg.Button("Paper", key="paper")
-scissors_button = sg.Button("Scissors", key="scissors")
-show_score_button = sg.Button("Show score", key="score")
-exit_button = sg.Button("Exit", key="exit")
-result = sg.Text("", key="output")
-check_win_lose = sg.Text("", key="check")
-
-layout = [[label], [[stone_button, paper_button, scissors_button]],
-          [show_score_button, result], [exit_button]]
-
-window = sg.Window("Stone-Paper-Scissor-APP", layout=layout, size= (400, 200),
-                   element_justification='c')
-
-
-
-
-window.close()
-
+master.mainloop()
