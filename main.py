03d24ef2ebaba58763ceb9459ef25fa03efe1e4c
i5-PC <mcienkowski@gmail.com>
2023-08-28 12:57:23 +0200

    Add all labels

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ master.title("RPS")
 #labels
 Label(master,text="rock-paper-scissors", font=("Clibri", 14)).grid(row=0, sticky=N,
         pady=10, padx=200)
-Label(master,text="Choose something", font=("Clibri", 14)).grid(row=0, sticky=N,
-        pady=10, padx=200)
+Label(master,text="Choose something", font=("Clibri", 12)).grid(row=1, sticky=N)
+player_score_label = Label(master,text="Player : 0", font=("Clibri", 12))
+player_score_label.grid(row=2, sticky=W)
+computer_score_label = Label(master,text="Computer : 0", font=("Clibri", 12))
+computer_score_label.grid(row=2, sticky=E)
+player_choice_label = Label(master,font=("Calibri", 12))
+player_choice_label.grid(row=3, sticky=W)
+computer_choice_label = Label(master,font=("Calibri", 12))
+computer_choice_label.grid(row=3, sticky=E)
+outcome_label = Label(master,font=("Calibri", 12))
+outcome_label.grid(row=3, sticky=N)
+
+
 
 master.mainloop()
